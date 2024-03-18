7426be14459930915a99c5296f61b101b136de3a
junseooo <pointjunseo@dgist.ac.kr>
2024-03-18 14:11:48 +0900

    change inBoxContent by title

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ function App() {
         <div className='container'>
           <span className='title'>상품등록</span>
           <div className='contents'>
-            <Selector title={'대분류'} need={true}/>
-            <Selector title={'중분류'} need={true}/>
-            <Print title={'상품코드'} need={true} comment={'※ 중분류 선택 후 자동으로 생성됩니다.'}/>
-            <Print title={'상품명'} need={true}/>
-            <Toggle title={'판매상태'} firstText={'판매중'} secondText={'판매중지'}/>
-            <Print title={'판매단가'} unit={'원'} need={false}/>
-            <Toggle title={'과세구분'} firstText={'과세'} secondText={'비과세'}/>
-            <Selector title={'재고단위'} need={true}/>
-            <Selector title={'발주단위'} need={false}/>
-            <Print title={'재고환산'}/>
+            <Selector title={'대분류'} need={true} />
+            <Selector title={'중분류'} need={true} />
+            <Print title={'상품코드'} need={true} comment={'※ 중분류 선택 후 자동으로 생성됩니다.'} />
+            <Print title={'상품명'} need={true} />
+            <Toggle title={'판매상태'} firstText={'판매중'} secondText={'판매중지'} />
+            <Print title={'판매단가'} unit={'원'} need={false} />
+            <Toggle title={'과세구분'} firstText={'과세'} secondText={'비과세'} />
+            <Selector title={'재고단위'} need={true} />
+            <Selector title={'발주단위'} need={false} />
+            <Print title={'재고환산'} content={'1.0'} />
           </div>
         </div>
       </div>
--- src/Component/Default/Default.js
+++ src/Component/Default/Default.js
@@ -7,6 +7,22 @@ function Default(props) {
   const handleChange = (e) => {
     setValue(e.target.value);
   }
+
+  function inBoxContent() {
+    if (title === '대분류' || title === '중분류' || title === '재고단위' || title === '발주단위') {
+      return <span className='default-box-content'>{content}</span>;
+    }
+    else if (title === '상품코드') {
+      return <span className='default-box-content'>blank</span>;
+    }
+    else if (title === '상품명' || title === '판매단가') {
+      return <input className='default-box-input' type='text' onChange={handleChange} />;
+    }
+    else if (title === '재고환산') {
+      return <input className='default-box-input' type='text' onChange={handleChange} placeholder='1.0' />;
+    }
+  }
+
   return (
     <div className='default-container'>
       <div className='default-title-container'>
@@ -15,11 +31,7 @@ function Default(props) {
         {comment && <span className='default-title-comment'>{comment}</span>}
       </div>
       <div className='default-box'>
-        {arrow || comment ?
-          <span className='default-box-content'>{content}</span> :
-          <input className='default-box-input' type='text' onChange={handleChange}
-          />
-        }
+        {inBoxContent()}
         {unit && <span className='default-box-content'>{unit}</span>}
         {arrow}
       </div>
